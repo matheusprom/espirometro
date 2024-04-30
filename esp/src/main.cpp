0ff814fbde7ba950db10a800cac8da17e2649128
matheusprom <matheuspromxx@gmail.com>
2024-04-30 10:41:03 -0300

    Implementado leitura do sensor de pressão.

--- esp/src/main.cpp
+++ esp/src/main.cpp
@@ -1,15 +1,21 @@
 #include <stdio.h>
 #include "HX710B.h"
 
-#define HX710B_DOUT 2
-#define HX710B_CLK  3
+#define HX710B_DOUT 13
+#define HX710B_CLK  12
 #define HX710B_GAIN 128
 HX710B pressure_sensor;
 
+// MAGIC NUMBERS
+#define OFFSET  540000 // used for tare the weight
+#define RES     2.98023e-7
 
 void sendSensorData(void)
 {
-  printf("%f", pressure_sensor.pascal());
+  // convert the raw read to pascal using magic numbers
+  float read_pascal =  ( (pressure_sensor.read() - OFFSET) * RES) *20 - 50;
+  printf("%X\n", read_pascal);
+  delay(0);
 }
 
 void setup(void)
